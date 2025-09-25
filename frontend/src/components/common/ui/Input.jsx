5b60a74ac7dd1438420a428f1ddfb5f00b65c168
Rachel Xing <xing0023@algonquinlive.com>
2025-09-25 16:45:40 -0400

    fix(ui component): solved Select and Input component error message alignment issue

--- frontend/src/components/common/ui/Input.jsx
+++ frontend/src/components/common/ui/Input.jsx
@@ -3,10 +3,10 @@ import { InputText } from 'primereact/inputtext'
 function Input ({...props}) {
     const {label,name,id,errors=[]} = {...props}
     return (
-        <div>
+        <div className='relative'>
             <label htmlFor={id} className='block text-sm font-medium mb-2'>{label}</label>
             <InputText {...props} className='w-full'/>
-            { errors[name] && <p className="text-red-500 text-sm mt-2">{ errors[name] }</p> }
+            { errors[name] && <p className="absolute text-red-500 text-sm mt-2">{ errors[name] }</p> }
         </div>
     )
 }
--- frontend/src/components/common/ui/Select.jsx
+++ frontend/src/components/common/ui/Select.jsx
@@ -4,12 +4,12 @@ import { Dropdown } from 'primereact/dropdown'
 function Select (props) {
     const { name, id, value,label, onChange, options,placeholder='Select an option',errors=[] } = { ...props }
     return (
-        <div>
+        <div className='relative'>
             <label htmlFor={ id } className="block text-sm font-medium mb-2">
                 {label }
             </label>
             <Dropdown {...props} className='w-full'/>
-            { errors[name] && <p className="text-red-500 text-sm mt-2">{ errors[name] }</p> }
+            { errors[name] && <p className="absolute text-red-500 text-sm mt-2">{ errors[name] }</p> }
         </div>
     )
 }
