0466120b957ac32bc10f37146fafa3d8be7cd52a
Rachel Xing <xing0023@algonquinlive.com>
2026-01-29 16:08:53 -0500

    feat: update tags handling in expense update and improve localization for tags placeholder

--- frontend/src/components/feature/claims/TagMultiSelect.jsx
+++ frontend/src/components/feature/claims/TagMultiSelect.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { MultiSelect } from 'primereact/multiselect'
 import { useLookups } from '../../../contexts/LookupContext.jsx'
 import { useTranslation } from 'react-i18next'
+import { useTags } from '../../../contexts/TagContext.jsx'
 
 export default function TagMultiSelect({ value, onChange }) {
     const { t } = useTranslation()
     const { lookups, loading } = useLookups()
+    const { tags } = useTags()
 
     return (
         <div className="w-80">
             <MultiSelect
                 value={value}
-                options={lookups.tags}
+                options={tags}
                 optionLabel="tag_name"
                 optionValue="tag_id"
                 onChange={e => onChange(e.value)}
