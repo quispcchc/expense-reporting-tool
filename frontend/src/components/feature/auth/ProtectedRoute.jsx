723985753358daaa1a56a6bea79cbee52828f8f4
Rachel Xing <xing0023@algonquinlive.com>
2026-02-23 16:12:44 -0500

    fix: loading issue

--- frontend/src/components/feature/auth/ProtectedRoute.jsx
+++ frontend/src/components/feature/auth/ProtectedRoute.jsx
@@ -1,13 +1,19 @@
 import { useAuth } from '../../../contexts/AuthContext.jsx'
 import { Navigate, useLocation } from 'react-router-dom'
 import Unauthorized from '../../../pages/shared/Unauthrized.jsx'
+import Loader from '../../common/ui/Loader.jsx'
 
 function ProtectedRoute({ children, allowedRoles = [] }) {
-    const { authUser, isAuthenticated } = useAuth()
+    const { authUser, isAuthenticated,isLoading } = useAuth()
     const location = useLocation()
 
     // If user is not authenticated, redirect to login page,
     // passing the current location for redirect after login
+
+    if (isLoading) {
+        return <Loader />
+    }
+    
     if (!isAuthenticated()) {
         return (<Navigate to="/login" state={{ from: location }} replace />)
     }
